0f90eb4c1cbd1732eb7889ef1a97299115b9f72c
unknown <harikarthikronaldo@gmail.com>
2023-03-27 09:00:49 +0530

    new files have been added
    use these files

---
+++ tfliteinferencing.py
@@ -0,0 +1,78 @@
+import tensorflow as tf
+import cv2
+import numpy as np
+import time
+# Load the Keras model
+model = tf.keras.models.load_model('C:\\Users\\SM Harikarthik\\Documents\\FaceDetection\\facetracker.h5')
+
+# Convert the model to TFLite format
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+tflite_model = converter.convert()
+
+with open('face_detection_model.tflite', 'wb') as f:
+    f.write(tflite_model)
+
+import tensorflow as tf
+import numpy as np
+import cv2
+
+# Load the TFLite model
+interpreter = tf.lite.Interpreter(model_path='face_detection_model.tflite')
+interpreter.allocate_tensors()
+
+# Get input and output tensors
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+print(input_details[0]['index'])
+print(output_details[0]['shape'])
+print(output_details[1]['shape'])
+
+
+cap = cv2.VideoCapture(0)
+while cap.isOpened():
+    _ , frame = cap.read()
+    frame = frame[50:500, 50:500,:]
+    
+    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    resized = tf.image.resize(rgb, (120,120))
+    img = np.expand_dims(resized/255,0)
+    
+
+    interpreter.set_tensor(input_details[0]['index'], img)
+
+    time1 = time.time()
+    interpreter.invoke()
+    time2 = time.time()
+    time3 = time2 - time1
+    print(time3)
+
+    yhat_img = interpreter.get_tensor(output_details[0]['index'])
+    yhat_coords = interpreter.get_tensor(output_details[1]['index'])
+    yhat = yhat_img,yhat_coords
+    sample_coords = yhat[1][0]
+
+    if yhat[0] > 0.5: 
+        # Controls the main rectangle
+        cv2.rectangle(frame, 
+                      tuple(np.multiply(sample_coords[:2], [450,450]).astype(int)),
+                      tuple(np.multiply(sample_coords[2:], [450,450]).astype(int)), 
+                            (255,0,0), 2)
+        # Controls the label rectangle
+        cv2.rectangle(frame, 
+                      tuple(np.add(np.multiply(sample_coords[:2], [450,450]).astype(int), 
+                                    [0,-30])),
+                      tuple(np.add(np.multiply(sample_coords[:2], [450,450]).astype(int),
+                                    [80,0])), 
+                            (255,0,0), -1)
+        
+        # Controls the text rendered
+        cv2.putText(frame, 'face', tuple(np.add(np.multiply(sample_coords[:2], [450,450]).astype(int),
+                                               [0,-5])),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
+    
+    cv2.imshow('EyeTrack', frame)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
